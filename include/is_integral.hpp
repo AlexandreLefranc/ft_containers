bbe2a0f19a031a94e18cb0e4e79364d8a5949763
Alex <alefranc@student.42.fr>
2022-10-26 18:31:34 +0200

    is_integral

--- include/is_integral.hpp
+++ include/is_integral.hpp
@@ -6,7 +6,7 @@
 /*   By: alefranc <alefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 14:14:14 by alefranc          #+#    #+#             */
-/*   Updated: 2022/10/26 18:22:59 by alefranc         ###   ########.fr       */
+/*   Updated: 2022/10/26 18:30:23 by alefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,104 @@ namespace ft
 		operator value_type() const { return value; }
 	};
 
+	// Specialization alias
 	typedef ft::integral_constant<bool, true>	true_type;
 	typedef ft::integral_constant<bool, false>	false_type;
 
+	// Any non specialized type -> false_type
 	template <class T>
-	struct is_integral: public ft::integral_constant<T>
-	{
-		
-	};
+	struct is_integral: public false_type {};
 
+	// List of specialized type -> true_type
+	template <>
+	bool
+
+	template <>
+	char
+
+	template <>
+	char16_t
+
+	template <>
+	char32_t
+
+	template <>
+	wchar_t
+
+	template <>
+	signed char
+
+	template <>
+	short int
+
+	template <>
+	int
+
+	template <>
+	long int
+
+	template <>
+	long long int
+
+	template <>
+	unsigned char
+
+	template <>
+	unsigned short int
+
+	template <>
+	unsigned int
+
+	template <>
+	unsigned long int
+
+	template <>
+	unsigned long long int
+
+	template <>
+	bool
+
+	template <>
+	char
+
+	template <>
+	char16_t
+
+	template <>
+	char32_t
+
+	template <>
+	wchar_t
+
+	template <>
+	signed char
+
+	template <>
+	short int
+
+	template <>
+	int
+
+	template <>
+	long int
+
+	template <>
+	long long int
+
+	template <>
+	unsigned char
+
+	template <>
+	unsigned short int
+
+	template <>
+	unsigned int
+
+	template <>
+	unsigned long int
+
+	template <>
+	unsigned long long int
 
 }
 
